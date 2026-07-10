7e82b3623b6634c7c3dd6ea1adaae86a7604678a
Dong_awa <2099467463@qq.com>
2026-07-10 12:42:29 +0800

    0.2.3-shot-260709b

--- src/main/java/bili/dongsz/broadcastradio/registry/ModCreativeModeTabs.java
+++ src/main/java/bili/dongsz/broadcastradio/registry/ModCreativeModeTabs.java
@@ -15,33 +15,38 @@ public class ModCreativeModeTabs {
 
     public static final RegistryObject<CreativeModeTab> BROADCAST_RADIO_TAB = CREATIVE_MODE_TABS.register("broadcast_radio_tab",
             () -> CreativeModeTab.builder()
-                    .icon(() -> new ItemStack(ModItems.COPPER_CONDUCTIVE_SLICE.get()))
+                    .icon(() -> new ItemStack(ModItems.SIMPLE_RADIO.get()))
                     .title(Component.translatable("itemGroup.broadcast_radio"))
                     .displayItems((parameters, output) -> {
-                        // 功能物品/方块
-                        output.accept(ModItems.PORTABLE_WALKIE_TALKIE.get());
-                        output.accept(ModItems.ENCRYPTED_WALKIE_TALKIE.get());
+                        // 功能方块
                         output.accept(ModItems.SIMPLE_RADIO.get());
                         output.accept(ModItems.RADIO_BASE_STATION.get());
-                        output.accept(ModItems.RADIO_TERMINAL.get());
                         output.accept(ModItems.SIMPLE_SIGNAL_JAMMER.get());
 
-                        // 原材料
-                        output.accept(ModItems.COPPER_CONDUCTIVE_SLICE.get());
-                        output.accept(ModItems.INSULATED_RUBBER.get());
-                        output.accept(ModItems.PCB_BOARD.get());
-                        output.accept(ModItems.ELECTRONIC_COMPONENT.get());
-                        output.accept(ModItems.RADIO_MODULE.get());
+                        // 功能物品
+                        output.accept(ModItems.PORTABLE_WALKIE_TALKIE.get());
+                        output.accept(ModItems.ENCRYPTED_WALKIE_TALKIE.get());
+                        output.accept(ModItems.RADIO_TERMINAL.get());
+
+                        //中间品
                         output.accept(ModItems.RADIO_COMPONENT.get());
-                        output.accept(ModItems.ANTENNA.get());
                         output.accept(ModItems.CIRCUIT_BOARD.get());
-                        
+                        output.accept(ModItems.ANTENNA.get());
+                        output.accept(ModItems.PCB_BOARD.get());
+
                         // 消耗品
-                        output.accept(ModItems.RADIO_BATTERY.get());
                         output.accept(ModItems.STORAGE_BATTERY.get());
+                        output.accept(ModItems.RADIO_BATTERY.get());
                         output.accept(ModItems.TWO_G_UNIVERSAL.get());
                         output.accept(ModItems.THREE_G_UNIVERSAL.get());
                         output.accept(ModItems.FOUR_G_UNIVERSAL.get());
+
+                        // 原材料
+                        output.accept(ModItems.RADIO_MODULE.get());
+                        output.accept(ModItems.ELECTRONIC_COMPONENT.get());
+                        output.accept(ModItems.COPPER_CONDUCTIVE_SLICE.get());
+                        output.accept(ModItems.INSULATED_RUBBER.get());
+
                     })
                     .build()
     );
