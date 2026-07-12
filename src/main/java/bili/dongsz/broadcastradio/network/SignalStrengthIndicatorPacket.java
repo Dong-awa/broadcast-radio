a074fe9011971b6119056e3e1b5943771406b6f7
Dong_awa <2099467463@qq.com>
2026-07-12 23:51:14 +0800

    0.2.3

--- src/main/java/bili/dongsz/broadcastradio/network/SignalStrengthIndicatorPacket.java
+++ src/main/java/bili/dongsz/broadcastradio/network/SignalStrengthIndicatorPacket.java
@@ -7,6 +7,9 @@ import net.minecraftforge.network.NetworkEvent;
 
 import java.util.function.Supplier;
 
+import bili.dongsz.broadcastradio.BroadcastRadio;
+import bili.dongsz.broadcastradio.client.SignalStrengthHUD;
+
 /**
  * 客户端收到此包后在快捷栏上方显示信号强度/质量指示。
  */
@@ -17,16 +20,28 @@ public class SignalStrengthIndicatorPacket {
     public SignalStrengthIndicatorPacket(int signalStrength, int interference) {
         this.signalStrength = signalStrength;
         this.interference = interference;
+        BroadcastRadio.LOGGER.debug(
+            "[SignalStrengthIndicatorPacket] 服务端构造数据包：strength={}, interference={}",
+            signalStrength, interference
+        );
     }
 
     public SignalStrengthIndicatorPacket(FriendlyByteBuf buf) {
         this.signalStrength = buf.readInt();
         this.interference = buf.readInt();
+        BroadcastRadio.LOGGER.debug(
+            "[SignalStrengthIndicatorPacket] 客户端从缓冲区解码：strength={}, interference={}",
+            this.signalStrength, this.interference
+        );
     }
 
     public void encode(FriendlyByteBuf buf) {
         buf.writeInt(signalStrength);
         buf.writeInt(interference);
+        BroadcastRadio.LOGGER.debug(
+            "[SignalStrengthIndicatorPacket] 编码到缓冲区：strength={}, interference={}",
+            signalStrength, interference
+        );
     }
 
     public static SignalStrengthIndicatorPacket decode(FriendlyByteBuf buf) {
@@ -34,11 +49,16 @@ public class SignalStrengthIndicatorPacket {
     }
 
     public static void handle(SignalStrengthIndicatorPacket packet, Supplier<NetworkEvent.Context> ctx) {
-        ctx.get().enqueueWork(() -> {
+        NetworkEvent.Context context = ctx.get();
+        context.enqueueWork(() -> {
             DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-                bili.dongsz.broadcastradio.client.SignalStrengthHUD.updateSignal(packet.signalStrength, packet.interference);
+                BroadcastRadio.LOGGER.info(
+                    "[SignalStrengthIndicatorPacket] 客户端收到信号指示包：strength={}, interference={}",
+                    packet.signalStrength, packet.interference
+                );
+                SignalStrengthHUD.updateSignal(packet.signalStrength, packet.interference);
             });
         });
-        ctx.get().setPacketHandled(true);
+        context.setPacketHandled(true);
     }
 }
